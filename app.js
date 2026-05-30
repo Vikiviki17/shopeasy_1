3e2b5fe6ee8247a26dc6d80f03e303ad656ac7c1
Vikiviki17 <Vikiviki17@gmail.com>
2026-05-30 20:55:27 +0530

    add test and ci pipeline

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.get('/', (req, res) => {
   res.send('i love you riya my soulmate') ;
 })
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
+
+module.exports= {app, server}
